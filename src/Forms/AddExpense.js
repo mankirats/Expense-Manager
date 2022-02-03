90ca60f6a39d09e71540bf3f64a3fd52a115df2c
Mankirat <mankirat95@gmail.com>
2022-02-03 21:39:40 +0530

    Form input field changes

--- src/Forms/AddExpense.js
+++ src/Forms/AddExpense.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function addExpense(props) {
+function AddExpense(props) {
+    let [expenseTitle, setExpenseTitle] = useState("");
+    let [expenseTotal, setExpenseTotal] = useState("");
+    let [expenseDate, setExpenseDate] = useState("");
+
+    const submitForm = (e) => {
+        console.log(`${expenseTitle} ${expenseTotal} ${expenseDate}`);
+        e.preventDefault();
+    };
     return (
         <>
-            <form action="post">
-                <input type="text" name="title" id="title" />
-                <input type="number" name="total" id="total" />
-                <input type="date" name="date" id="date" />
+            <form onSubmit={(e) => submitForm(e)}>
+                <input
+                    type="text"
+                    name="title"
+                    id="title"
+                    value={expenseTitle}
+                    onChange={(e) => setExpenseTitle(e.target.value)}
+                />
+
+                <input
+                    type="number"
+                    name="total"
+                    id="total"
+                    value={expenseTotal}
+                    onChange={(e) => setExpenseTotal(e.target.value)}
+                />
+
+                <input
+                    type="date"
+                    name="date"
+                    id="date"
+                    value={expenseDate}
+                    onChange={(e) => setExpenseDate(e.target.value)}
+                />
+
                 <button type="submit">Add Expense</button>
             </form>
         </>
     );
 }
 
-export default addExpense;
+export default AddExpense;
