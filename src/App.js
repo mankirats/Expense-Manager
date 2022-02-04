11d70baabcc6c778e994e6eec47be8b507935389
Mankirat <mankirat95@gmail.com>
2022-02-04 08:00:37 +0530

    Add expense state lifted

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import AddExpense from "./Forms/AddExpense";
 const AppContainer = styled.div`
     display: flex;
     justify-content: center;
+    flex-wrap: wrap;
 `;
 
 const ExpenseContainerDiv = styled.div`
     margin: 1rem 0;
 `;
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -33,6 +35,20 @@ class App extends Component {
 
         // ];
     }
+
+    submitForm = (event) => (formState) => {
+        event.preventDefault();
+        this.setState(
+            this.state.expenseData.push({
+                id:
+                    this.state.expenseData[this.state.expenseData.length - 1]
+                        .id + 1,
+                title: formState.expenseTitle,
+                total: formState.expenseTotal,
+                date: formState.expenseDate,
+            })
+        );
+    };
     render() {
         return (
             <AppContainer>
@@ -43,6 +59,7 @@ class App extends Component {
                         title="TITLE"
                         total="TOTAL"
                         date="DATE"
+                        submitForm={(data) => this.submitForm(data)}
                     ></Expense>
                     {this.state.expenseData.map((expenseItem) => {
                         return (
