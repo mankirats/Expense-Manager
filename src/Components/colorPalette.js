c904194c691b00133e44f034801609a33bfdf03a
Mankirat <mankirat95@gmail.com>
2022-02-10 20:22:27 +0530

    palette fixed

--- src/Components/colorPalette.js
+++ src/Components/colorPalette.js
@@ -1,5 +1,6 @@
 export const colorPalette = {
-    color1: "#6886C5",
+    // color1: "#6886C5",
+    color1: "#2C394B",
     // dark blue
     color2: "#FFACB7",
     // orange
@@ -8,4 +9,7 @@ export const colorPalette = {
     color4: "#F9F9F9",
     // Yellow
     color5: "#FFFFFF",
+    // white
+    errorCol: "#c23b22",
+    // Pastel red
 };
