4c3ebff10b0294673796cfcb062c20febd38592f
Mankirat <mankirat95@gmail.com>
2022-03-14 22:15:33 +0530

    user router changes

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const router = new express.Router();
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 
-router.get("/api/v1/register", (req, res) => {
-    res.send("hello");
+router.get("/api/v1/userprofile", auth, (req, res) => {
+    const user = req.user;
+    res.send(user);
 });
 
 router.post("/api/v1/register", async (req, res) => {
     try {
         const user = new User(req.body);
         const result = await user.save();
-        res.send(result);
+        const token = result.generateAuthToken();
+        res.send({
+            status: 200,
+            data: result,
+            generatedToken: token,
+        });
     } catch (err) {
         res.send(err.message);
     }
