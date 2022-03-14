e5852b2a85f67fb52f3da291f5e25fd696ed6efa
Mankirat <mankirat95@gmail.com>
2022-03-14 22:14:49 +0530

    User model modified for token field

--- models/user.js
+++ models/user.js
@@ -1,31 +1,57 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("../db/mongoose");
 
-const userSchema = new mongoose.Schema(
+const userSchema = mongoose.Schema(
     {
         name: {
             type: String,
-            required: true,
+            require: true,
         },
-        email: {
-            type: String,
-            required: true,
-            validate: (email) => {
-                return String(email)
-                    .toLowerCase()
-                    .match(
-                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                    );
-            },
-        },
+        email: { type: String, index: true, unique: true, required: true },
         password: {
             type: String,
             required: true,
         },
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    required: true,
+                },
+            },
+            (validate = function (val) {
+                return val.length > 1;
+            }),
+        ],
     },
     { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.generateAuthToken = function () {
+    const user = this;
+    try {
+        const token = jwt.sign(
+            { id: user._id.toString() },
+            process.env.JWT_SECRET
+        );
+        user.tokens = user.tokens.concat({ token });
+        user.save();
+        return token;
+    } catch (err) {
+        console.log(err.message);
+    }
+};
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
