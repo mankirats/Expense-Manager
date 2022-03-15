38d5e3d940e44a4569a75c3f3ee18d3113fa60db
Mankirat <mankirat95@gmail.com>
2022-03-15 23:04:46 +0530

    authentication added

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,10 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const auth = async function (req, res, next) {
-    const token = req.header("authorization").replace("Bearer ", "");
+    const token = ("Bearer " || req.header("authorization")).replace(
+        "Bearer ",
+        ""
+    );
     const decodeToken = jwt.decode(token, process.env.JWT_SECRET);
     const user = await User.findOne({
         id: decodeToken,
