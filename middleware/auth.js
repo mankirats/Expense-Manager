6d8f734630865768657a93ea43b44f1e0bc47c21
Mankirat <mankirat95@gmail.com>
2022-03-16 12:44:26 +0530

    authentication changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,9 +6,11 @@ const auth = async function (req, res, next) {
     const token = req.header("authorization").replace("Bearer ", "");
     console.log(`auth: ${token}`);
     const verifyToken = await jwt.verify(token, process.env.JWT_SECRET);
+
     if (!verifyToken) {
         throw new Error("Please authenticate");
     }
+
     const decodeToken = await jwt.decode(token, { complete: true });
     const user = await User.findOne({
         id: decodeToken.payload.id,
