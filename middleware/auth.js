66660927a67a89e24d154cef73540c0cc25cd6d9
Mankirat <mankirat95@gmail.com>
2022-03-16 20:05:15 +0530

    auth middle fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,22 +3,20 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const auth = async function (req, res, next) {
-    const token = req.header("authorization").replace("Bearer ", "");
+    const token = req.header("Authorization").replace("Bearer ", "");
     console.log(`auth: ${token}`);
-    const verifyToken = await jwt.verify(token, process.env.JWT_SECRET);
-
-    if (!verifyToken) {
-        throw new Error("Please authenticate");
-    }
-
-    const decodeToken = await jwt.decode(token, { complete: true });
+    const decodeToken = await jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decodeToken);
+    console.log(decodeToken.id);
+    console.log(token);
     const user = await User.findOne({
-        id: decodeToken.payload.id,
+        _id: decodeToken.id,
         "tokens['token']": token,
     });
 
-    console.log(user);
+    // console.log(user);
     req.user = user;
+    console.log(req.user);
     next();
 };
 
