e644c7166b9767246c30ede48163c5febd7d1c70
Mankirat <mankirat95@gmail.com>
2022-03-17 22:51:02 +0530

    Minor fixes in the server js and auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
-
+require("../db/mongoose");
 const auth = async function (req, res, next) {
     try {
         const token = req.header("Authorization").replace("Bearer ", "");
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const User = require("./models/user");
 // require("dotenv").config({ path: "/config/dev.env" });
 const userRouter = require("./routers/userRouter");
+const expenseRouter = require("./routers/expenseRouter");
 const port = process.env.PORT;
-
+require("./db/mongoose");
 app.use(express.json());
 app.use(userRouter);
+app.use(expenseRouter);
 
 app.listen(port, () => {
     console.log("Server is running at port - " + port);
