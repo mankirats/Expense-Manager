d0462d3fe99ae8be355e036cdb7b0842536a8f88
Mankirat <mankirat95@gmail.com>
2022-03-19 09:21:56 +0530

    New Route added for expense patch

--- routers/expenseRouter.js
+++ routers/expenseRouter.js
@@ -16,7 +16,7 @@ router.get("/api/v1/expenses", auth, async (req, res) => {
     }
 });
 
-router.post("/api/v1/task/add", auth, async (req, res) => {
+router.post("/api/v1/expense/add", auth, async (req, res) => {
     try {
         const expense = new Expense({
             ...req.body,
@@ -26,6 +26,51 @@ router.post("/api/v1/task/add", auth, async (req, res) => {
         res.send(result);
     } catch (err) {
         res.send(err.message);
+    }
+});
+
+router.patch("/api/v1/expense/:id/update", auth, async (req, res) => {
+    try {
+        const expense_object = await Expense.findOne({});
+        const allowed_updates = Object.keys(expense_object.toJSON()).slice(
+            1,
+            -4
+        );
+        const req_updates = Object.keys(req.body);
+
+        const expense = await Expense.findOne({
+            _id: req.params.id.toString(),
+            expensedBy: req.user._id,
+        });
+        console.log(expense);
+        if (!expense) {
+            throw new Error("Expense not available");
+        }
+
+        const isValidUpdate = req_updates.every((key) =>
+            allowed_updates.includes(key)
+        );
+
+        if (!isValidUpdate) {
+            throw new Error(
+                `Requested updates are not valid. Allowed updates are listed below:${allowed_updates}`
+            );
+        }
+
+        for (x of req_updates) {
+            expense[x] = req.body[x];
+        }
+        const updatedExpense = await expense.save();
+
+        res.status(201).send({
+            status: 201,
+            data: updatedExpense,
+        });
+    } catch (err) {
+        res.status(400).send({
+            status: 400,
+            message: err.message,
+        });
     }
 });
 
